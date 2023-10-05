e9a09ffeeb270b9e2b8d50a1cbbc0a7472dae7d1
Raphael Bonfim <raphael.bonfim.fer@gmail.com>
2023-10-05 03:07:40 -0300

    Primeiro commit

--- Exe01/Program.cs
+++ Exe01/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace Exe01;
+﻿/* 
+ 
+Ler dez elementos de uma matriz unidimensional do tipo inteiro e apresentar os valores
+lidos
+
+*/
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Clear();
+
+        var array = new int[10];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"Digite 10 numeros inteiros [{i + 1}] = ");
+
+            array[i] = int.Parse(Console.ReadLine()!);
+            Console.Clear();
+
+            Console.WriteLine($"Numero digitado: {array[i]}");
+
+            Thread.Sleep(600);
+            Console.Clear();
+        }
+
+
+        Console.Write($"Valores no Array : ");
+
+        foreach (int numero in array)
+        {
+            Console.Write($"{numero} ");
+        }
+
+        Console.ReadKey();
     }
 }
